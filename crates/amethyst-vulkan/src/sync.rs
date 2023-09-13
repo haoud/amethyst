24967d026eaec3cba9cf7b2d508255e9f1c95be7
haoud <cadilhac.romain@gmail.com>
2023-09-13 19:57:32 +0200

    Added Fence support

--- crates/amethyst-vulkan/src/sync.rs
+++ crates/amethyst-vulkan/src/sync.rs
@@ -46,3 +46,122 @@ impl Drop for Semaphore {
         }
     }
 }
+
+/// A fence is a CPU-GPU synchronization primitive that can be used to insert a
+/// dependency from a queue to the host.
+pub struct Fence {
+    device: Arc<RenderDevice>,
+    inner: vk::Fence,
+}
+
+impl Fence {
+    /// Creates a new fence with the given flags.
+    #[must_use]
+    pub fn new(device: Arc<RenderDevice>, flags: FenceCreateFlags) -> Self {
+        let info = vk::FenceCreateInfo::builder()
+            .flags(flags.into())
+            .build();
+
+        let inner = unsafe {
+            device
+                .logical()
+                .inner()
+                .create_fence(&info, None)
+                .expect("Failed to create fence")
+        };
+
+        Self { device, inner }
+    }
+
+    /// Query the current status of the fence without resetting it no
+    /// waiting for it.
+    pub fn query(&self) -> FenceStatus {
+        let status = unsafe {
+            self.device
+                .logical()
+                .inner()
+                .get_fence_status(self.inner)
+                .expect("Failed to query fence status")
+        };
+
+        match status {
+            vk::SuccessCode::NOT_READY => FenceStatus::Unsignaled,
+            vk::SuccessCode::SUCCESS => FenceStatus::Signaled,
+            _ => panic!("Unexpected fence status: {:?}", status),
+        }
+    }
+
+    /// Reset the fence to the unsignaled state.
+    pub fn reset(&self) {
+        unsafe {
+            self.device
+                .logical()
+                .inner()
+                .reset_fences(&[self.inner])
+                .expect("Failed to reset fence");
+        }
+    }
+
+    /// Wait for the fence to be signaled. This function will block the current
+    /// thread until the fence is signaled without a timeout.
+    pub fn wait(&self) {
+        unsafe {
+            self.device
+                .logical()
+                .inner()
+                .wait_for_fences(&[self.inner], true, u64::MAX)
+                .expect("Failed to wait for fence");
+        }
+    }
+
+    /// Return the inner vulkan fence.
+    pub(crate) fn inner(&self) -> vk::Fence {
+        self.inner
+    }
+}
+
+impl Drop for Fence {
+    fn drop(&mut self) {
+        unsafe {
+            self.device
+                .logical()
+                .inner()
+                .destroy_fence(self.inner, None);
+        }
+    }
+}
+
+/// Configuration used to create a fence.
+pub struct FenceCreateFlags {
+    /// Specifies that the fence object is created in the signaled state.
+    /// Otherwise, it is created in the unsignaled state.
+    /// If a fence is created in the signaled state, applications can wait for
+    /// it without delay assuming they have not reset it since it was signaled.
+    pub signaled: bool,
+}
+
+impl Default for FenceCreateFlags {
+    fn default() -> Self {
+        Self {
+            signaled: false,
+        }
+    }
+}
+
+impl From<FenceCreateFlags> for vk::FenceCreateFlags {
+    fn from(infos: FenceCreateFlags) -> Self {
+        let mut flags = vk::FenceCreateFlags::empty();
+        if infos.signaled {
+            flags |= vk::FenceCreateFlags::SIGNALED;
+        }
+        flags
+    }
+}
+
+
+/// The status of a fence.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum FenceStatus {
+    Unsignaled,
+    Signaled,
+}
